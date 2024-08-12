1dbfead6303394c1cef439771be94d3c638f6428
adrianH0498 <202011420@urp.edu.pe>
2024-08-11 19:36:10 -0500

    chore: Add Font Awesome stylesheet and link to GitHub repository

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Menu({ onSelectGame }) {
   return (
-    <div className="container text-center mt-5" style={{ backgroundColor: '#F2F2F2', padding: '20px', borderRadius: '10px' }}>
+    <div className="container text-center mt-5" style={{ backgroundColor: '#F2F2F2', padding: '20px', borderRadius: '10px', maxWidth: '500px', margin: 'auto' }}>
       <h1 style={{ color: '#F23E16' }}>Bienvenido al Juego de Fiesta</h1>
       <p className="lead" style={{ color: '#F27052' }}>Elige tu juego:</p>
       <button className="btn m-2" onClick={() => onSelectGame('yoNunca')} style={{ backgroundColor: '#F23E16', color: '#F2F2F2', borderRadius: '5px', padding: '10px 20px' }}>
@@ -11,6 +11,11 @@ function Menu({ onSelectGame }) {
       <button className="btn m-2" onClick={() => onSelectGame('juegoDeCartas')} style={{ backgroundColor: '#F27052', color: '#F2F2F2', borderRadius: '5px', padding: '10px 20px' }}>
         Juego de Cartas
       </button>
+      <div style={{ marginTop: '20px' }}>
+        <a href="https://github.com/adrian04981/ChupiSe-al" target="_blank" rel="noopener noreferrer" style={{ color: '#F23E16', fontSize: '1.5em' }}>
+          <i className="fab fa-github"></i> Visita mi repositorio
+        </a>
+      </div>
     </div>
   );
 }
